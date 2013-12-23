dad79ae5ecdb5a1683d431261bfd83c47d65a016
Ritvvij Parrikh <ritvij.j@gmail.com>
2013-12-23 15:40:59 +0530

    Delayed Job added

--- db/migrate/20131001130537_core.rb
+++ db/migrate/20131001130537_core.rb
@@ -135,22 +135,6 @@ class Core < ActiveRecord::Migration
       t.datetime "updated_at",  :null => false
     end
 
-    create_table "delayed_jobs", :force => true do |t|
-      t.integer  "priority",   :default => 0, :null => false
-      t.integer  "attempts",   :default => 0, :null => false
-      t.text     "handler",                   :null => false
-      t.text     "last_error"
-      t.datetime "run_at"
-      t.datetime "locked_at"
-      t.datetime "failed_at"
-      t.string   "locked_by"
-      t.string   "queue"
-      t.datetime "created_at",                :null => false
-      t.datetime "updated_at",                :null => false
-    end
-
-    add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
-
     create_table "permissions", :force => true do |t|
       t.integer  "user_id"
       t.integer  "account_id"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131001130537) do
+ActiveRecord::Schema.define(:version => 20131223092339) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
