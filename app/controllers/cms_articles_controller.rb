98ae3f284f18d500fc569c87607ecc927f2fdd25
Ritvvij Parrikh <ritvij.j@gmail.com>
2013-12-24 23:14:47 +0530

    cms artciles fix controller

--- app/controllers/cms_articles_controller.rb
+++ app/controllers/cms_articles_controller.rb
@@ -1,95 +1,67 @@
 class CmsArticlesController < ApplicationController
-  # GET /cms_articles
-  # GET /cms_articles.json
+  
+  before_filter :authenticate_user!, except: [:show, :index]
+  before_filter :find_objects
+  before_filter :authorize, except: [:index, :show]
+
   def index
-    @cms_articles = Cms::Article.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @cms_articles }
-    end
+    @cms_articles = @account.cms_articles
   end
 
-  # GET /cms_articles/1
-  # GET /cms_articles/1.json
   def show
-    @cms_article = Cms::Article.find(params[:file_id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @cms_article }
-    end
   end
 
-  # GET /cms_articles/new
-  # GET /cms_articles/new.json
   def new
     @cms_article = Cms::Article.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @cms_article }
-    end
   end
 
-  # GET /cms_articles/1/edit
   def edit
-    @cms_article = Cms::Article.find(params[:file_id])
   end
 
-  # POST /cms_articles
-  # POST /cms_articles.json
   def create
-    
     @cms_article = Cms::Article.new(params[:cms_article])
-    
     @cms_article.is_published = false
     if params[:commit] == "Publish"
       @cms_article.is_published = true
     end
     @cms_article.description.to_s.html_safe
-    respond_to do |format|
-      if @cms_article.save
-        format.html { redirect_to user_account_cms_articles_path, notice: 'Cms article was successfully created.' }
-        format.json { render json: @cms_article, status: :created, location: user_account_cms_articles_path }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @cms_article.errors, status: :unprocessable_entity }
-      end
+    if @cms_article.save
+      redirect_to user_account_cms_articles_path(@account.owner, @account.slug, file_id: @cms_article.slug), notice: t("c.s")
+    else
+      render action: "new"
     end
   end
 
-  # PUT /cms_articles/1
-  # PUT /cms_articles/1.json
   def update
-    @cms_article = Cms::Article.find(params[:file_id])
-
     @cms_article.is_published = false
     if params[:commit] == "Publish"
       @cms_article.is_published = true
     end
-
     @cms_article.description.to_s.html_safe
-    respond_to do |format|
-      if @cms_article.update_attributes(params[:cms_article])
-        format.html { redirect_to user_account_cms_articles_path, notice: 'Cms article was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @cms_article.errors, status: :unprocessable_entity }
-      end
+    if @cms_article.update_attributes(params[:cms_article])
+      redirect_to user_account_cms_article_path(@account.owner, @account.slug, file_id: @cms_article.slug), notice: t("u.s")
+    else
+      render action: "edit"
     end
   end
 
-  # DELETE /cms_articles/1
-  # DELETE /cms_articles/1.json
   def destroy
-    @cms_article = Cms::Article.find(params[:file_id])
     @cms_article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to user_account_cms_articles_path, notice: "Record Successfully Deleted" }
-      format.json { head :no_content }
+    redirect_to user_account_cms_articles_path(@account.owner, @account.slug)
+  end
+  
+  private
+  
+  def find_objects
+    if params[:file_id].present? 
+      @cms_article = @account.cms_articles.find(params[:file_id])
     end
   end
+  
+  def authorize
+    if !@is_admin
+      redirect_to root_url, error: "Permission denied."
+    end
+  end
+  
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -18,6 +18,7 @@ class Account < ActiveRecord::Base
   has_many :data_filzs, class_name: "Data::Filz", dependent: :destroy
   has_many :api_filzs, class_name: "Api::Filz", dependent: :destroy
   has_many :viz_vizs, class_name: "Viz::Viz", dependent: :destroy
+  has_many :cms_articles, class_name: "Cms::Article", dependent: :destroy
 
   #VALIDATIONS
   validates :name, presence: true, length: {minimum: 5}, on: :create
