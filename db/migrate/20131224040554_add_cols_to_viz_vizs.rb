2db2f1f1e2f796378340729173f6c00949096c8e
afzal-lakdawala <al@pykih.com>
2013-12-24 20:06:07 +0530

    Gitignore file error solved

--- db/migrate/20131224040554_add_cols_to_viz_vizs.rb
+++
@@ -1,7 +0,0 @@
-class AddColsToVizVizs < ActiveRecord::Migration
-  def change
-    add_column :viz_vizs, :created_by, :integer
-    add_column :viz_vizs, :updated_by, :integer
-    add_column :viz_vizs, :account_id, :integer
-  end
-end
