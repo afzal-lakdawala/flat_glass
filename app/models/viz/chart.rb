7bc22f56fe5704620e64d4248ed72974d0733df7
Ritvvij Parrikh <ritvij.j@gmail.com>
2014-01-01 16:20:14 +0530

    committing temporarily the mapper #35 still incomplete

--- app/models/viz/chart.rb
+++ app/models/viz/chart.rb
@@ -28,8 +28,7 @@ class Viz::Chart < ActiveRecord::Base
     headings = raw_data.shift
     headings = headings.collect{|h| h.split(":").first}
     map_json = JSON.parse(viz.map).invert
-
-    if self.genre == CHART_1D
+    if self.genre == CHART_1D or self.genre == CHART_2D or self.genre == CHART_W2D or self.genre == CHART_GS2D
       Viz::Chart.mapper_1d_or_2d(genre, raw_data, headings, map_json)
     elsif self.genre == CHART_WT
       Viz::Chart.mapper_unweighted_tree(viz)
@@ -37,12 +36,6 @@ class Viz::Chart < ActiveRecord::Base
       Viz::Chart.mapper_weighted_tree(viz)
     elsif self.genre == CHART_RELATION
       Viz::Chart.mapper_relations(viz)
-    elsif self.genre == CHART_2D
-      Viz::Chart.mapper_1d_or_2d(genre, raw_data, headings, map_json)
-    elsif self.genre == CHART_W2D
-      Viz::Chart.mapper_weighted_2d(raw_data, headings, map_json)
-    elsif self.genre == CHART_GS2D
-      Viz::Chart.mapper_groupedstacked_2d(raw_data, headings, map_json)
     end
   end
   
@@ -53,22 +46,29 @@ class Viz::Chart < ActiveRecord::Base
     raw_data.each do |row|
       if genre == CHART_1D
         label = row[headings.index(map_json["Dimension"])]
-        value = row[headings.index(map_json["Size"])]
-      elsif genre == CHART_2D
-        label = row[headings.index(map_json["X"])]
-        value = row[headings.index(map_json["Y"])]
+      elsif genre == CHART_2D or CHART_W2D or CHART_GS2D
+        label = row[headings.index(map_json["X"])]        
       end
-      h[label] = h[label].present? ? (h[label].to_f + value.to_f) : value.to_f
+      group = map_json["Group"].present? ? row[headings.index(map_json["Group"])]     : "_"
+      stack = map_json["Stack"].present? ? row[headings.index(map_json["Stack"])]     : "_"
+      size  = map_json["Size"].present?  ? row[headings.index(map_json["Size"])].to_f : 0.0
+      y     = map_json["Y"].present?     ? row[headings.index(map_json["Y"])].to_f    : 0.0
+      unique_label = "#{label}#{group}#{stack}"
+      if h[unique_label].present?
+        h[unique_label] = {"label" => label, "group" => group, "stack" => stack, "y" => h[unique_label]["y"].to_f + y.to_f, "size" => h[unique_label]["size"].to_f + size.to_f }
+      else
+        h[unique_label] = {"label" => label, "group" => group, "stack" => stack, "y" => y.to_f, "size" => size.to_f}
+      end
     end
     if h != {}
-      h.each do |key, val|
-        out << [key, val]
+      h.each do |unique_label, label, group, stack, size, y|
+        out << [label, group, stack, size, y]
       end
       transformed_data[0]["values"].push(out)
     end  
     transformed_data.to_json
   end
-  
+    
   def self.mapper_weighted_tree
   end
   
@@ -77,58 +77,7 @@ class Viz::Chart < ActiveRecord::Base
   
   def self.mapper_relations
   end
-  
-  def self.incomplete(raw_data, headings, map_json)
-    transformed_data = [{"key" => "Chart","values" => []}] #json_data
-    h = {}
-    out = []
-    raw_data.each do |row|
-      label = row[headings.index(map_json["X"])]
-      value = row[headings.index(map_json["Y"])]
-      group  = row[headings.index(map_json["Group"])]
-      stack  = row[headings.index(map_json["Stack"])]
-      unique_label = label
-      unique_label = "#{unique_label}_" + group if group.present?
-      unique_label = "#{unique_label}_" + stack if stack.present?
-      if h[unique_label].present?
-        h[unique_label] = {"x" => h[unique_label]["x"], "y" => h[unique_label]["y"].to_f + value.to_f, "group" => h[unique_label]["group"], "stack" => h[unique_label]["stack"]}
-      else        
-        h[unique_label] = {"x" => h[unique_label]["x"], "y" => h[unique_label]["y"].to_f + value.to_f, "group" => h[unique_label]["group"], "stack" => h[unique_label]["stack"]}
-        #{"y" => value.to_f, "size" => size.to_f}
-      end
-    end
-    if h != {}
-      h.each do |key, val, size|
-        out << [key, val, size]
-      end
-      transformed_data[0]["values"].push(out)
-    end  
-    transformed_data.to_json    
-  end
-  
-  def self.mapper_weighted_2d(raw_data, headings, map_json)
-    transformed_data = [{"key" => "Chart","values" => []}] #json_data
-    h = {}
-    out = []
-    raw_data.each do |row|
-      label = row[headings.index(map_json["X"])]
-      value = row[headings.index(map_json["Y"])]
-      size  = row[headings.index(map_json["Size"])]
-      if h[label].present?
-        h[label] = {"y" => h[label]["y"].to_f + value.to_f, "size" => h[label]["size"].to_f + size.to_f }
-      else        
-        h[label] = {"y" => value.to_f, "size" => size.to_f}
-      end
-    end
-    if h != {}
-      h.each do |key, val, size|
-        out << [key, val, size]
-      end
-      transformed_data[0]["values"].push(out)
-    end  
-    transformed_data.to_json    
-  end   
-  
+      
   #UPSERT
   #JOBS
   #PRIVATE
